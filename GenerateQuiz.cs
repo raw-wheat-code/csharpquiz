a295198a3ece06703379ff493c7853ba7a30a631
Ross Wheatley <rossw1416@gmail.com>
2024-04-06 20:49:57 -0400

    Commit

--- GenerateQuiz.cs
+++ GenerateQuiz.cs
@@ -6,12 +6,10 @@ namespace csharpquiz
     // Class to load questions from json into a Quiz list
     public class GenerateQuiz
     {
-        public void Generate() {
-
-        }
-
+        // Will load questions for a quiz, like:
+        // private Quiz quiz = GenerateQuiz.LoadQuizData();
         public List<Question> LoadQuizData() {
-
+            
             return new List<Question>();
         }
 
