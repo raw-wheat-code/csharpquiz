b8cf8c1f1a8966136af51bc7133ac6c7b15c6c12
cn0ck5 <52336650+cn0ck5@users.noreply.github.com>
2024-04-12 17:24:47 -0400

    updated comment

--- Question.cs
+++ Question.cs
@@ -6,7 +6,7 @@ namespace csharpquiz
     public class Question
     {
         public string question; // Full text of question to display to the console
-        public string correct; // Single letter corresponding to the correct answer
+        public string correct; // String corresponding to the correct answer
         public List<string> answers = new List<string>(); // Possible answers to display to the console
         public int group; // identifier assigned to groups of questions that are entered at the same time
     }
