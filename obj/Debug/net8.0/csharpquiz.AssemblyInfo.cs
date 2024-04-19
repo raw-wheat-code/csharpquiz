498fc30c3a795a9251f58164daabe57601a092e6
Ross Wheatley <rossw3653@gmail.com>
2024-04-19 15:45:42 -0400

    asdasdf

--- obj/Debug/net8.0/csharpquiz.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharpquiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharpquiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f96b21719e37442a9e971c4e28bec824cc7a4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669fd2010af868b4fc802405d6555e20256e002d")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharpquiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharpquiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
